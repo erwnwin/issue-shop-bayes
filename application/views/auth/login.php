ec261d736b9a37f342d96cdd22b0fe8b32a52966
Erwin, S.Kom <84768767+erwnwin@users.noreply.github.com>
2025-01-21 16:09:28 +0800

    Update login.php

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -118,7 +118,7 @@
 <script src="<?= base_url() ?>public/assets/plugins/sweetalert2/sweetalert2.min.js"></script>
 <!-- Toastr -->
 <script src="<?= base_url() ?>public/assets/plugins/toastr/toastr.min.js"></script>
-<script src="<?= base_url() ?>public/css/login.js"></script>
+<!-- <script src="<?= base_url() ?>public/css/login.js"></script> -->
 <script src="<?= base_url() ?>public/assets/js/login.js"></script>
 
 </html>
